4902705d013bfc58d195b2af531b9aad83f23b63
Todd838 <todd.perkins@bison.howard.edu>
2026-09-01 14:02:40 -0400

    Add developer profile information

---
+++ app.py
@@ -0,0 +1,11 @@
+name = "Todd Perkins"
+major = "Computer Science"
+technology_area_interest = "Agentic AI"
+skill_goal = "Increased expertise in full stack development"
+
+print("Senior Project Developer Profile")
+print()
+print("Name:", name)
+print("Major:", major)
+print("Technology Area Interest:", technology_area_interest)
+print("Skill Goal:", skill_goal)
